03d20a9c407f7666abf57525e374201127d44a56
Alvin Crespo <alvin.crespo@gmail.com>
2020-01-20 21:09:02 -0500

    getting some initial scenarios in

--- index.js
+++ index.js
@@ -1,3 +1,98 @@
 #!/usr/bin/env node
 
-console.log("Dev Trail!");
+console.log("===============Dev Trail===============");
+
+const inquirer = require("inquirer");
+
+const questions = [
+  {
+    type: "rawlist",
+    name: "scenario",
+    message: "Choose your background.",
+    choices: [
+      {
+        key: "c",
+        name: "College",
+        value: "college"
+      },
+      {
+        key: "b",
+        name: "Bootcamp",
+        value: "bootcamp"
+      },
+      {
+        key: "s",
+        name: "Self-taught",
+        value: "selftaught"
+      }
+    ]
+  },
+  {
+    type: "rawlist",
+    name: "gender",
+    message: "Choose your gender.",
+    choices: [
+      {
+        key: "n",
+        name: "Non-binary",
+        value: "nonbinary"
+      },
+      {
+        key: "f",
+        name: "Female",
+        value: "female"
+      },
+      {
+        key: "m",
+        name: "Male",
+        value: "male"
+      }
+    ]
+  },
+  {
+    type: "rawlist",
+    name: "title",
+    message: "What's your job title?",
+    choices: [
+      {
+        key: "j",
+        name: "Junior Engineer",
+        value: "juniorengineer"
+      },
+      {
+        key: "s1",
+        name: "Software Engineer",
+        value: "softwareengineer"
+      },
+      {
+        key: "s2",
+        name: "Senior Software Engineer",
+        value: "senioreengineer"
+      },
+      {
+        key: "em",
+        name: "Engineering Manager",
+        value: "manager"
+      },
+      {
+        key: "de",
+        name: "Director of Engineering",
+        value: "director"
+      },
+      {
+        key: "vp",
+        name: "Vice President of Engineering",
+        value: "vicepresident"
+      },
+      {
+        key: "cto",
+        name: "Chief Technology Officer",
+        value: "cto"
+      }
+    ]
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log({ answers });
+});
